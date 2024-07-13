4ef76e4cf962f11aedbfcc5b959f71fd9dca1d52
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2024-07-13 14:52:44 -0400

    sfg

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,23 +6,56 @@ public class GameManager : MonoBehaviour
 {
     private Spawner spawner;
     public GameObject title;
+    private Vector2 screenBounds;
+    public GameObject playerPrefab;
+    private GameObject player;
+    private bool gameStarted = false;
+
     private void Awake()
     {
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        player = playerPrefab;
     }
 
     // Start is called before the first frame update
     void Start()
     {
         spawner.active = false;
+        title.SetActive(true);
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (!gameStarted)
         {
-            spawner.active = true;
+            if (Input.anyKeyDown)
+            {
+                spawner.active = true;
+                title.SetActive(false);
+            }
         }
+
+
+        var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+
+        foreach (GameObject bombObject in nextBomb)
+        {
+            if (bombObject.transform.position.y < (-screenBounds.y) - 12)
+            {
+                Destroy(bombObject);
+            }
+        }
+
     }
+
+    void ResetGame()
+    {
+        spawner.active = true;
+        title.SetActive
+    }
+
 }
